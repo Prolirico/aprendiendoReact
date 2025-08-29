230e6c6b3757817d30f358105c019fe00b7260e2
Axel <Prolirico1@gmail.com>
2025-08-29 09:32:28 -0600

    Correccion de aprobacion de cursos a los alumnos

--- backend/middleware/authMiddleware.js
+++ backend/middleware/authMiddleware.js
@@ -22,7 +22,7 @@ const protect = async (req, res, next) => {
 
       // Obtener datos del usuario desde la BD para asegurar que existe y tener datos frescos
       const [users] = await pool.query(
-        "SELECT id_usuario, username as nombre, email, tipo_usuario FROM usuario WHERE id_usuario = ?",
+        "SELECT id_usuario, username, email, tipo_usuario FROM usuario WHERE id_usuario = ?",
         [decoded.id_usuario],
       );
 
